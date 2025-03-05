e5c279fafe804f5478c3d8f921edf3949636b69a
ArquimedesAlexandre <arquimedesalexandre5@gmail.com>
2025-03-05 11:15:25 +0100

    none
    indroduzindo js

--- js/index.js
+++ js/index.js
@@ -1,3 +1,14 @@
+//variaveis e seleçao de elementos
+ const apikey = "6ca08a75c339068d89f60222c9b06ab5";
+ const apicountryurl = "https://flagsapi.com/BE/flat/64.png";
+ 
+ 
+ const cityinput = document.querySelector("#city-input");
+ const searchBtn = document.querySelector("#search");
 
-// const apikey = 6ca08a75c339068d89f60222c9b06ab5
-// const apicountryurl =
+ // eventos
+ searchBtn.addEventListener("click",(e) =>{
+    const city = cityinput.value;
+    e.preventDefault();
+    console.log(city);
+ });
